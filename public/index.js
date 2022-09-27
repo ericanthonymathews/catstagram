2a6c511e0fc30df5c903d5ef2e06f467de141ecf
Shana Edouard <shanaedouard06@gmail.com>
2022-09-27 15:24:10 -0700

    finished phase 1

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-// Your code here
+// const { brotliDecompressSync } = require("zlib");
+
+window.onload = async () => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await res.json();
+    const url = data[0].url
+    console.log(url)
+    const h1 = document.createElement("h1");
+    h1.innerText = "Kitten Pic";
+    const img = document.createElement("img");
+    img.setAttribute("src", url);
+    document.body.append(h1, img);
+}
